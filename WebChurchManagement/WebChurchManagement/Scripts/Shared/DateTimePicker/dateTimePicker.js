3c58c42ce6b685a9b689837957463b02ac03936b
dorighetto <leonardo.dorighetto@gmail.com>
2017-05-17 10:39:53 -0300

    Inclusão do datetime picker nas views de create e edit de membros.

---
+++ WebChurchManagement/WebChurchManagement/Scripts/Shared/DateTimePicker/dateTimePicker.js
@@ -0,0 +1,15 @@
+﻿//Para mais configurações do datetime picker visitar o seguinte endereço:
+//https://uxsolutions.github.io/bootstrap-datepicker/?#sandbox
+
+$(document).ready(function () {
+
+    $('.dateTimePicker').datepicker({
+        format: "dd/mm/yyyy",
+        todayBtn: "linked",
+        clearBtn: true,
+        language: "pt-BR",
+        autoclose: true,
+        todayHighlight: true
+    });
+
+});
