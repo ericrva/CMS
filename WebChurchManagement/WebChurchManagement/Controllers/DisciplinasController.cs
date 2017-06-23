dc91dfa2a4f925d0651fb0c1195f6eada5ee4ed2
Leonardo <leonardo.dorighetto@gmail.com>
2017-06-23 16:33:57 -0300

    Iniciando telas de disciplinas.

---
+++ WebChurchManagement/WebChurchManagement/Controllers/DisciplinasController.cs
@@ -0,0 +1,132 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebChurchManagement.Models;
+
+namespace WebChurchManagement.Controllers
+{
+    [RoutePrefix("Disciplinas")]
+    public class DisciplinasController : Controller
+    {
+        private WebChurchManagementContext db = new WebChurchManagementContext();
+
+        // GET: Disciplinas
+        public ActionResult Index()
+        {
+            return View(db.Disciplinas.ToList());
+        }
+
+        // GET: Disciplinas/Detalhes/5
+        [Route("Detalhes/{id}")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // GET: Disciplinas/Create
+        [Route("Inserir")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Disciplinas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Inserir")]
+        public ActionResult Create([Bind(Include = "Id_Disc,Id_Memb,Motivo,Dt_Ini,Dt_Fim")] Disciplina disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Disciplinas.Add(disciplina);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(disciplina);
+        }
+
+        // GET: Disciplinas/Edit/5
+        [Route("Editar/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // POST: Disciplinas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Editar/{id}")]
+        public ActionResult Edit([Bind(Include = "Id_Disc,Id_Memb,Motivo,Dt_Ini,Dt_Fim")] Disciplina disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(disciplina).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(disciplina);
+        }
+
+        // GET: Disciplinas/Delete/5
+        [Route("Deletar/{id}")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            if (disciplina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disciplina);
+        }
+
+        // POST: Disciplinas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Route("Deletar/{id}")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Disciplina disciplina = db.Disciplinas.Find(id);
+            db.Disciplinas.Remove(disciplina);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
